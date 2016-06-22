05b2310aafa296e0ed4658cfc76411835e55aca3
Jacob Hafdahl <j.hafdahl@sportradar.com>
2016-06-22 17:04:47 -0500

    in progress commit

--- lib/professor_stats.ex
+++ lib/professor_stats.ex
@@ -2,17 +2,23 @@ defmodule ProfessorStats do
 	use Application
 	require Logger
 
-	def start(_type, _args) do
+	def start(_type, args) do
 		import Supervisor.Spec
 
 		children = [
 			supervisor(ProfessorStats.Repo,[]),
 			supervisor(ProfessorStats.CalcService,[]),
-			supervisor(ProfessorStats.ScheduleService,[])
+			supervisor(ProfessorStats.ScheduleService,[]),
+			worker(Task, [ProfessorStats, :loop, args])
 		]
 
 		opts = [strategy: :one_for_one, name: ProfessorStats.Supervisor]
 
 		Supervisor.start_link(children, opts)
 	end
+
+		# def loop do
+
+		# 	loop()
+		# end
 end
